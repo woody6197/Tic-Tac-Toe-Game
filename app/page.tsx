39b65d1374b62d078be2445cceaf1424608f7eb3
woody6197 <woody.burgess@uk.ey.com>
2025-12-02 16:19:42 +0000

    Update padding

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-col-3 min-h-screen w-full justify-between items-center py-16 px-20 sm:px-8 bg-slate-950">
+      <main className="flex flex-col-3 min-h-screen w-full justify-between items-center py-4 px-4 md:py-16 md:px-20 bg-slate-950">
         <Image
           className="hidden lg:block"
           src="/transparent-logo.png"
